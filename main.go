f6e1c7b364be33c5856755f1537f5a7fd0354273
Nikhil Prabhu <nikhilprabhu00@gmail.com>
2020-11-19 16:21:25 +0530

    Add server init code

--- main.go
+++ main.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+        "./routes"
+        "./controllers"
+        "./utils"
+        "log"
+        "net/http"
+        "os"
+        "os/signal"
+        "syscall"
+
+        "github.com/joho/godotenv"
+)
+
 func main() {
-        return
+        // Handle server termination
+        handleInterrupts()
+
+        log.Println("Starting server...")
+        // Close DB connections when server is killed
+        defer utils.ClosePostgreClient(controllers.PDB)
+        defer utils.CloseMongoClient(controllers.MDB, controllers.Ctx)
+
+        // Load environment
+        err := godotenv.Load()
+        if err != nil {
+                log.Fatalln(err)
+        }
+        // Port to run server on
+        port := os.Getenv("PORT")
+
+        // Handle routes
+        http.Handle("/", routes.Handlers())
+
+        // Serve
+        log.Println("Server listening on port:", port)
+        log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+// handleInterrupts handles server termination
+// in a somewhat graceful way. It handles the
+// SIGINT and SIGTERM signals.
+func handleInterrupts() {
+        // Channel to notify about received signal
+        ch := make(chan os.Signal)
+        // Notify received signal
+        signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+
+        // Goroutine that blocks till signal is
+        // received on channel
+        go func() {
+                <- ch
+                log.Println("Server terminated. Exiting...")
+                os.Exit(0)
+        }()
 }
