nikhil-prabhu/dynosurveys-backend

commit 19b25e6336422fb149eda2ff5e188d2c48c64114

Make handleInterrupts handle all signals

main.go
@@ -39,13 +39,15 @@ func main() {
 }
 
 // handleInterrupts handles server termination
-// in a somewhat graceful way. It handles the
-// SIGINT and SIGTERM signals.
+// in a somewhat graceful way.
 func handleInterrupts() {
-        // Channel to notify about received signal
-        ch := make(chan os.Signal)
+        // Channel to notify about received signal.
+        // We use a buffered channel to make sure
+        // that it's ready to receive when a signal
+        // is sent
+        ch := make(chan os.Signal, 1)
         // Notify received signal
-        signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+        signal.Notify(ch)
 
         // Goroutine that blocks till signal is
         // received on channel

commit 4a7ee20ed6b0c7cf05c4f2202e5c3d11088bad30

Handle only SIGINT and SIGTERM signals
    This commit fixes an issue that was introduced by commit 19b25e6,
    where the server would randomly stop running (presumably because of
    receiving some signal during execution that wasn't manually sent by
    the user).

main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 
 	"github.com/joho/godotenv"
 	"github.com/nikhil-prabhu/dynosurveys-backend/controllers"
@@ -40,7 +41,8 @@ func main() {
 }
 
 // handleInterrupts handles server termination
-// in a somewhat graceful way.
+// in a somewhat graceful way. It handles the
+// SIGINT and SIGTERM signals.
 func handleInterrupts() {
         // Channel to notify about received signal.
         // We use a buffered channel to make sure
@@ -48,7 +50,7 @@ func handleInterrupts() {
         // is sent
         ch := make(chan os.Signal, 1)
         // Notify received signal
-        signal.Notify(ch)
+        signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
 
         // Goroutine that blocks till signal is
         // received on channel